4e04241b1a3a054ab1e3e927b13f1cce16c9ba7e
Ian Brettell <brettell@ebi.ac.uk>
2020-09-16 16:32:38 +1000

    -m

--- code/scripts/20200916_abbababa_functions.R
+++ code/scripts/20200916_abbababa_functions.R
@@ -79,3 +79,122 @@ block_jackknife <- function(block_indices, FUN, ...){
 
     list(mean=mean, std_dev=std_dev)
     }
+
+run_abbababa <- function(data, P1, P2){
+
+  # set popns and remove NAs
+  P3 <- "mikk"
+  P3a <- "mikk_a"
+  P3b <- "mikk_b"
+  pops <- c(P1, P2, P3, P3a, P3b)
+
+  # Select only those populations and remove NAs
+  freq_table <- data %>%
+    dplyr::select(chr, pos, ancestral, derived, all_of(pops)) %>%
+    tidyr::drop_na()
+
+  # Create output list
+  out_list <- list()
+
+  # Add populations
+  out_list[["Populations"]] <- list("P1" = P1,
+                                    "P2" = P2,
+                                    "P3" = "mikk")
+
+  # Get genome-wide D stat
+  out_list[["Genome-wide D"]] <- list()
+
+  D <- D.stat(freq_table[,P1], freq_table[,P2], freq_table[,P3])
+  out_list[["Genome-wide D"]][["D statistic"]] <- D
+
+  block_indices <- get_block_indices(block_size=1e6, # Block jackknife to obtain SD and Z-score
+                                     positions=freq_table$pos,
+                                     chromosomes=freq_table$chr)
+
+  block_indices <- block_indices[lapply(block_indices, length) > 0 ]  # remove empty entries
+
+  n_blocks <- length(block_indices)
+
+  out_list[["Genome-wide D"]][["Number of blocks"]] <- n_blocks
+
+  D_sd <- get_jackknife_sd(block_indices=block_indices, # get D SD
+                         FUN=D.stat,
+                         freq_table[,P1], freq_table[,P2], freq_table[,P3])
+
+  out_list[["Genome-wide D"]][["Standard deviation"]] <- D_sd
+
+  D_err <- D_sd/sqrt(n_blocks)
+
+  D_Z <- D / D_err
+
+  out_list[["Genome-wide D"]][["Z score"]] <- D_Z
+
+  # Get admixture proportion
+  out_list[["Admixture"]] <- list()
+
+  f <- f.stat(freq_table[,P1], freq_table[,P2], freq_table[,P3a], freq_table[,P3b])
+
+  out_list[["Admixture"]][["f statistic"]] <- f
+
+  f_sd <- get_jackknife_sd(block_indices=block_indices,
+                         FUN=f.stat,
+                         freq_table[,P1], freq_table[,P2], freq_table[,P3a], freq_table[,P3b])
+
+  out_list[["Admixture"]][["Standard deviation"]] <- f_sd
+
+  f_err <- f_sd/sqrt(n_blocks)
+
+  f_CI_lower <- f - 1.96*f_err
+  f_CI_upper <- f + 1.96*f_err
+
+  out_list[["Admixture"]][["Confidence interval"]] <- list("lower" = f_CI_lower,
+                                                           "upper" = f_CI_upper)
+
+  # Get per-chromosome D
+  chrom_names <- unique(freq_table$chr)
+  chrom_indices <- lapply(chrom_names, function(chrom) which(freq_table$chr == chrom))
+  names(chrom_indices) <- chrom_names
+
+  out_list[["Per-chromosome D"]] <- lapply(chrom_names, function(chrom){
+    per_chr_out <- list()
+    # get D stat
+    D_by_chrom <- D.stat(freq_table[chrom_indices[[chrom]], P1],
+                                           freq_table[chrom_indices[[chrom]], P2],
+                                           freq_table[chrom_indices[[chrom]], P3])
+
+    per_chr_out[["D statistic"]] <- D_by_chrom
+
+    # number of SNPs
+    per_chr_out[["Number of SNPs"]] <- length(chrom_indices[[chrom]])
+
+    # get block indices
+    block_indices_by_chrom <- get_block_indices(block_size=1e6,
+                                       positions=freq_table$pos[freq_table$chr == chrom])
+    # remove empty blocks
+    block_indices_by_chrom <- block_indices_by_chrom[lapply(block_indices_by_chrom, length) > 0 ]
+
+    # Get SD
+    D_sd_by_chrom <- get_jackknife_sd(block_indices=block_indices_by_chrom,
+                                      FUN=D.stat,
+                                      freq_table[chrom_indices[[chrom]], P1],
+                                      freq_table[chrom_indices[[chrom]], P2],
+                                      freq_table[chrom_indices[[chrom]], P3])
+
+    per_chr_out[["Standard error"]] <- D_sd_by_chrom
+
+    # Get Z score
+    D_err_by_chrom <- D_sd_by_chrom / sqrt(length(block_indices_by_chrom))
+
+    D_Z_by_chrom <- D_by_chrom / D_err_by_chrom
+
+    per_chr_out[["Z score"]] <- D_Z_by_chrom
+
+    # Return list
+    return(per_chr_out)
+  })
+
+  names(out_list[["Per-chromosome D"]]) <- chrom_names
+
+  # return output
+  return(out_list)
+}
