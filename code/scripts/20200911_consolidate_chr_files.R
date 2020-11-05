b42b399a1722d0cce4667658f41776dc257dfee8
Ian Narain Brettell <brettell@hx-noah-07-14.ebi.ac.uk>
2020-11-05 21:47:44 +0000

    20201106 update from cluster

--- code/scripts/20200911_consolidate_chr_files.R
+++ code/scripts/20200911_consolidate_chr_files.R
@@ -8,33 +8,33 @@ library(tidyverse)
 
 # Collect variables from bash script
 
-chr <- args[1]
-dir_in <- args[2]
-dir_out <- args[3]
+chr = args[1]
+dir_in = args[2]
+dir_out = args[3]
 
 # Set new variables
 
-chr_prefix <- paste(chr, "_", sep = "")
+chr_prefix = paste(chr, "_", sep = "")
 
 # Get list of files
 
-files <- list.files(dir_in, pattern = chr_prefix, full.names = T)
+files = list.files(dir_in, pattern = chr_prefix, full.names = T)
 
 # read data files into list
 
-dat_list <- lapply(files, function(x){
+dat_list = lapply(files, function(x){
   # read in data
-  df <- read.delim(x, header = T)
+  df = read.delim(x, header = T)
   return(df)
 })
 
 # set names
 
-names(dat_list) <- stringr::str_split(basename(files), pattern = "\\.", simplify = T)[, 1]
+names(dat_list) = stringr::str_split(basename(files), pattern = "\\.", simplify = T)[, 1]
 
 # bind, sort rows, and re-order columns
 
-dat_df <- dplyr::bind_rows(dat_list, .id = "chr_file") %>% # bind into single DF
+dat_df = dplyr::bind_rows(dat_list, .id = "chr_file") %>% # bind into single DF
   dplyr::arrange(coord) %>%  # sort by coordinate
   dplyr::select(chr_pos, # order columns
                 chr,
@@ -46,10 +46,10 @@ dat_df <- dplyr::bind_rows(dat_list, .id = "chr_file") %>% # bind into single DF
                 oryzias_javanicus,
                 oryzias_melastigma,
                 chr_file) %>%
-  dplyr::mutate(across(everything(), ~na_if(.x, "-"))) # substitute "-" with NA              
+  dplyr::mutate(across(everything(), ~na_if(.x, "-"))) # substitute "-" with NA
 
 # write table
 
-bname_out <- paste(chr, ".txt", sep = "")
-file_out <- file.path(dir_out, bname_out)
+bname_out = paste(chr, ".txt", sep = "")
+file_out = file.path(dir_out, bname_out)
 write.table(dat_df, file_out, quote = F, sep = "\t", row.names = F)
