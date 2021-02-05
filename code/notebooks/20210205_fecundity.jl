88ef99afe72d7a7dd6a6d872301658b041cae11f
brettellebi <brettell@noah-login-01.ebi.ac.uk>
2021-02-05 13:03:15 +0000

    20210205 update from cluster

---
+++ code/notebooks/20210205_fecundity.jl
@@ -0,0 +1,39 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ee9ea220-67b0-11eb-026f-752712eb67e0
+md"
+# Fecundity plots
+"
+
+# ╔═╡ fbeea218-67b0-11eb-026d-e3857af31f42
+md"
+## Setup
+"
+
+# ╔═╡ 00a95b4a-67b1-11eb-0ee6-eb06d4d310d3
+md"
+### Set working directory
+"
+
+# ╔═╡ 7feec574-67b0-11eb-3d29-6d6dff03ea05
+cd("/hps/research1/birney/users/ian/mikk_paper/mikk_genome")
+
+# ╔═╡ e12b13e8-67b1-11eb-04b2-5f5de4eab8a8
+md"
+### Read in data
+"
+
+# ╔═╡ 2ac85402-67b2-11eb-0fed-7389ff6d7f85
+
+
+# ╔═╡ Cell order:
+# ╠═ee9ea220-67b0-11eb-026f-752712eb67e0
+# ╠═fbeea218-67b0-11eb-026d-e3857af31f42
+# ╠═00a95b4a-67b1-11eb-0ee6-eb06d4d310d3
+# ╠═7feec574-67b0-11eb-3d29-6d6dff03ea05
+# ╠═e12b13e8-67b1-11eb-04b2-5f5de4eab8a8
+# ╠═2ac85402-67b2-11eb-0fed-7389ff6d7f85
