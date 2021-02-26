f511f83432cdcc2cd0b85955fd9bb6c033fe0ea0
brettellebi <brettell@ebi.ac.uk>
2021-02-26 14:07:39 +0000

    20210226 update from local

--- code/scripts/sv_analysis/source.R
+++ code/scripts/sv_analysis/source.R
@@ -5,14 +5,48 @@
 #############################
 
 library(tidyverse)
+library(scales)
 library(cowplot)
 library(circlize)
+library(karyoploteR)
+library(magick)
+library(plotly)
 
+
+#############################
+# Functions
+#############################
+
+round.choose <- function(x, roundTo, dir = 1) {
+  if(dir == 1) {  ##ROUND UP
+    x + (roundTo - x %% roundTo)
+  } else {
+    if(dir == 0) {  ##ROUND DOWN
+      x - (x %% roundTo)
+    }
+  }
+}
 
 #############################
 # Plotting
 #############################
 
+## Sample order
+
+ont_samples = c("4-1", "4-2", "7-1", "7-2", "11-1", "69-1", "79-2", "80-1", "117-2", "131-1", "134-1", "134-2")
+ont_samples_pol = c("4-1", "7-1", "11-1", "69-1", "79-2", "80-1", "117-2", "131-1", "134-1", "134-2")
+
 ## Palettes
 
 pal_paddle = c("#9b5de5","#f15bb5","#fee440","#00bbf9","#00f5d4")
+pal_brainbow = c("#264653","#2a9d8f","#e9c46a","#f4a261","#e76f51")
+pal_riviera = c("#05668d","#028090","#00a896","#02c39a","#f0f3bd")
+pal_lavender = c("#f2d7ee","#d3bcc0","#a5668b","#69306d","#0e103d")
+pal_smrarvo = c("#54478c","#2c699a","#048ba8","#0db39e","#16db93","#83e377","#b9e769","#efea5a","#f1c453","#f29e4c")
+pal_warmror = c("#5f0f40","#9a031e","#fb8b24","#e36414","#0f4c5c")
+pal_pastels = c("#ffadad","#ffd6a5","#fdffb6","#caffbf","#9bf6ff","#a0c4ff","#bdb2ff","#ffc6ff","#fffffc")
+
+
+
+
+
