95655cbd8cb2bf036207507a2a9160959a1a82f8
brettellebi <brettell@ebi.ac.uk>
2021-03-02 17:27:56 +0000

    20210302 update from local 2

--- code/scripts/repeats/source.R
+++ code/scripts/repeats/source.R
@@ -7,10 +7,32 @@
 library(here)
 library(tidyverse)
 library(scales)
+library(karyoploteR)
 library(circlize)
+
+#############################
+# Functions
+#############################
+
+round.choose <- function(x, roundTo, dir = 1) {
+  if(dir == 1) {  ##ROUND UP
+    x + (roundTo - x %% roundTo)
+  } else {
+    if(dir == 0) {  ##ROUND DOWN
+      x - (x %% roundTo)
+    }
+  }
+}
 
 #############################
 # Plotting
 #############################
+
+## Factor order
+
+chr_order = paste("chr", 1:24, sep = "")
+strand_order = c("+", "-")
+
+## Palettes
 
 pal_electroangler = c("#7400b8", "#6930c3", "#5e60ce", "#5390d9", "#4ea8de", "#48bfe3", "#56cfe1", "#64dfdf", "#72efdd", "#80ffdb")
