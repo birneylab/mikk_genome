1e1f4884322cd238819f9024cd595ca656012aea
brettellebi <brettell@noah-login-01.ebi.ac.uk>
2021-03-31 17:15:24 +0100

    20210331 update from cluster with final abba

---
+++ code/scripts/introgression/20210331_remove_sibs_2.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly = TRUE)
+
+# Import libraries
+
+library(tidyverse)
+
+# Collect variables from bash script
+
+in_file = args[1]
+samples_keep_file = args[2]
+out_file = args[3]
+
+# Read in files
+
+df = read.table(in_file,
+                header = T,
+                sep = "\t",
+                as.is = T,
+                comment.char = "*",
+                check.names = F)
+
+# Read in samples to keep
+
+samples_keep = scan(samples_keep_file, what = character())
+
+# Remove them
+df_nosibs = df %>%
+  dplyr::select("#CHROM", POS, all_of(samples_keep))
+
+# Write table
+
+write.table(df_nosibs, out_file, quote = F, sep = "\t", row.names = F)
