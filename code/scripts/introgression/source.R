9b769580a30e962d7affa77eeeb9ed22c0c9a115
brettellebi <brettell@ebi.ac.uk>
2021-04-29 19:06:04 +0100

    20210429 update from local

--- code/scripts/introgression/source.R
+++ code/scripts/introgression/source.R
@@ -39,5 +39,5 @@ fish_order <- c("MIKK", "HdrR", "HSOK", "HNI")
 
 # Palettes
 
-pal_abba <- c("#F3B61F", "#631E68", "#F6673A", "#F33A56", "#55B6B0", "#621B00")
-names(pal_abba) <- c("HdrR", "HSOK", "HNI", "melastigma", "javanicus", "Kaga")
+pal_abba <- c("#9E2B25", "#F3B61F", "#631E68", "#F6673A", "#F33A56", "#55B6B0", "#621B00")
+names(pal_abba) <- c("iCab", "HdrR", "HSOK", "HNI", "melastigma", "javanicus", "Kaga")
