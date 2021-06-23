e135bd815edebb47181939c3b3ac52500630bda7
Ian Brettell <brettell@ebi.ac.uk>
2021-06-23 20:55:09 +0100

    20210623 commit

---
+++ code/scripts/introgression/20210427_make_pop_file_all_except_no-kaga_no-sibs_no-131-1.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly = TRUE)
+
+# Import libraries
+
+library(tidyverse)
+
+# Collect variables from bash script
+
+in_file = args[1]
+out_file = args[2]
+
+# Full run
+
+emf_samples = c("hdrr", "hni", "hsok", "ancestor")
+vcf_samples = readr::read_tsv(in_file, col_names = "samples")
+
+vcf_samples$population = ifelse(vcf_samples$samples %in% emf_samples,
+                            vcf_samples$samples,
+                            ifelse(grepl("iCab", vcf_samples$samples),
+                                   "icab",
+                                   ifelse(grepl("Ho5", vcf_samples$samples),
+                                          "ho5",
+                                          ifelse(grepl("KW", vcf_samples$samples),
+                                                 "kiyosu_wild",
+                                                 "mikk"))))
+
+readr::write_tsv(vcf_samples, out_file)
